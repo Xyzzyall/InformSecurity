0890f3c42dbbaa7310a8732dea1fd39e8ee39be7
Xyzzyall <xyzzyall@gmail.com>
2019-11-30 14:21:01 +0300

    Char frequency analyse and decifer

--- Cifer/Caesar.py
+++ Cifer/Caesar.py
@@ -1,12 +1,12 @@
-RUS_QWERTY = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+import Cifer
 
 
 class Caesar:
     __textfile__ = None
     __shift__ = 0
-    __key__ = RUS_QWERTY
+    __key__ = ''
 
-    def __init__(self, textfile, shift, key=RUS_QWERTY):
+    def __init__(self, textfile, shift, key=Cifer.RUS_QWERTY):
         self.__textfile__ = textfile
         self.__shift__ = shift
         self.__key__ = key
@@ -14,13 +14,12 @@ class Caesar:
     def __iter__(self):
         f = open(self.__textfile__, 'r')
         for line in f:
-            res_line = ' '
+            res_line = ''
             for char in line:
-                capital = char in RUS_QWERTY.capitalize()
+                capital = char in self.__key__
                 try:
-                    i = self.__key__.index(char.lower()) + self.__shift__
-                    i %= len(RUS_QWERTY)
-                    res_line += RUS_QWERTY[i].capitalize() if capital else RUS_QWERTY[i]
+                    i = (self.__key__.index(char.lower()) + self.__shift__) % len(self.__key__)
+                    res_line += self.__key__[i].capitalize() if capital else self.__key__[i]
                 except ValueError:
                     res_line += char
             yield res_line
--- Cifer/__init__.py
+++ Cifer/__init__.py
@@ -1,1 +1,63 @@
 from Cifer.Caesar import Caesar as Caesar
+
+
+RUS_QWERTY = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+
+
+def char_frequency(textfile, bigramm=False):
+    res = {}
+    f = open(textfile, 'r')
+    for line in f:
+        bigramm_flipflop = True
+        buf = ''
+        for char in line:
+            if bigramm:
+                if bigramm_flipflop:
+                    buf = char
+                    bigramm_flipflop = False
+                    continue
+                else:
+                    buf += char
+                    bigramm_flipflop = True
+            else:
+                buf = char
+
+            if buf in res.keys():
+                res[buf] += 1
+            else:
+                res[buf] = 1
+    f.close()
+    return res
+
+
+def decifer(input_file, output_file, keys):
+    """Replaces characters from input_file by the 'keys' dictionary.
+    Writes results in output_file.
+    All 'file's are paths."""
+    inp = open(input_file, 'r')
+    out = open(output_file, 'w+')
+    for line in inp:
+        outline = ''
+        while line:
+            not_finded = True
+            for k in keys.keys():
+                try:
+                    if line.lower().index(k) == 0:
+                        outline += keys[k]
+                        line = line[len(k):]
+                        not_finded = False
+                        break
+                except ValueError:
+                    pass
+
+            if line and not_finded:
+                outline += line[0]
+                line = line[1:]
+
+        out.write(outline)
+    inp.close()
+    out.close()
+
+
+
+
