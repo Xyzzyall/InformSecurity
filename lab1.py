82d36d921e009c3f7b2e67f2f450b446ee4a600d
Xyzzyall <xyzzyall@gmail.com>
2019-11-30 16:16:43 +0300

    project better reorganized

--- lab1.py
+++ lab1.py
@@ -1,20 +1,22 @@
 import Cifer
 
 KEY = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+reference = 'Refs/test.txt'
+result_dir = 'lab1_out/'
 
-Cifer.cifer(Cifer.Caesar('test.txt', 1), 'out_test.txt')
+Cifer.cifer(Cifer.Caesar(reference, 1), result_dir+'out_test.txt')
 
-orig_chr_fr = Cifer.char_frequency('test.txt')
-coded_chr_fr = Cifer.char_frequency('out_test.txt')
+orig_chr_fr = Cifer.char_frequency(reference)
+coded_chr_fr = Cifer.char_frequency(result_dir+'out_test.txt')
 key = Cifer.find_a_caesar_key(orig_chr_fr, coded_chr_fr)
 print(key)
 
-Cifer.decifer('out_test.txt', 'out_decoded_test.txt', key)
+Cifer.decifer(result_dir+'out_test.txt', result_dir+'out_decoded_test.txt', key)
 
 
-orig_chr_fr = Cifer.char_frequency('test.txt', bigramm=True)
-coded_chr_fr = Cifer.char_frequency('out_test.txt', bigramm=True)
+orig_chr_fr = Cifer.char_frequency(reference, bigramm=True)
+coded_chr_fr = Cifer.char_frequency(result_dir+'out_test.txt', bigramm=True)
 key = Cifer.find_a_caesar_key(orig_chr_fr, coded_chr_fr)
 print(key)
 
-Cifer.decifer('out_test.txt', 'out_decoded_bigram_test.txt', key)
+Cifer.decifer(result_dir+'out_test.txt', result_dir+'out_decoded_bigram_test.txt', key)
